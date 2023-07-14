132d05d8964cb97e725dcfaf4f84edd2d591d103
Sara Zahedi <sara@Saras-MacBook-Pro-2.local>
2023-07-14 13:01:32 +0200

    add routes to projects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Login from './pages/Login';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter >
+      <Routes>
+            <Route path='/' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
